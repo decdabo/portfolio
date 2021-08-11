124c11b8502c81aa3eb3d9205d1b6d031d42a456
Dabo <david.caro.dec@hotmail.com>
2021-08-10 23:20:35 -0300

    Updates

--- src/components/contact/ContactScreen.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const ContactScreen = () => {
-    return (
-        <div>
-            <h1>El truenito pai</h1>
-        </div>
-    )
-}
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { PortApp } from './PortApp';
-
-ReactDOM.render(
-    <PortApp />,
-  document.getElementById( 'root' )
-);
