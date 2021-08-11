de2c2d0f830c7716b58b2827335adcb31f711ba2
Dabo <david.caro.dec@hotmail.com>
2021-08-10 22:23:01 -0300

    first commit

---
+++ src/components/home/HomeScreen.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+export const HomeScreen = () => {
+    return (
+        <div className="home__home-conteiner">
+            <div className="home__aside"/>
+            
+            <div className="home__home-main">
+
+                <div className="home__home-line-out"/>
+                
+                <div className="home__title-conteiner">
+                    <div className="home__name">
+                        <h1>David</h1>
+                    </div>
+                    <div className="home__lastname">Caro</div>
+                </div>
+
+                <div className="home__home-line-in" />
+
+                <div className="home__home-img-conteiner">
+                    <img 
+                        className="home__home-logo"
+                        src="./assets/logo/logo.png"
+                        alt="logo"/>
+                </div>
+
+                <div className="home__home-line-in" />
+
+                <div className="home__portfolio-box">
+                    <h2>Portfolio</h2>
+                </div>
+
+                <div className="home__home-line-out"/>
+
+            </div>
+
+            <div className="home__aside"/>
+            
+        </div>
+    )
+}
---
+++ src/components/nav/WorksApp.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { hideWork } from '../../helpers/hideWork'
+
+export const WorksApp = () => {
+
+    const handleWorkHide = () =>{
+
+        hideWork();
+
+    }
+
+    return (
+        <div className="work__work-conteiner" id="work__box">
+            <div className="work__work-conteiner-box">
+                <div className="work__work-main">
+
+                    <img 
+                        className="work__work-travolta"
+                        src="./assets/404/travolta-confundido.gif" alt="travolta" 
+                        />
+                
+                </div>
+
+                <div 
+                    className="work__work-button"
+                    onClick={handleWorkHide}
+                    >
+
+                    <i className="fas fa-angle-up"/>
+
+                </div>
+                
+            </div>
+        </div>
+    )
+}
